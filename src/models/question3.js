05fd039ef5d5302bdb3a63a51970ce8c91366adc
ManasaBsv <bsvmannu@gmail.com>
2020-02-25 13:35:47 +0530

    =changes

---
+++ src/models/question3.js
@@ -0,0 +1,43 @@
+const questions3=[
+    { question:'Question1',
+      optionA:'A',
+      optionB:'B',
+      optionC: 'C',
+      optionD: 'D',
+      correctAns:'optionA'
+    
+    },
+    { question:'Question2',
+      optionA:'A',
+      optionB:'B',
+      optionC: 'C',
+      optionD: 'D',
+      correctAns:'optionB'
+    
+    },
+    { question:'Question3',
+      optionA:'A',
+      optionB:'B',
+      optionC: 'C',
+      optionD: 'D',
+      correctAns:'optionC'
+    
+    },
+    { question:'Question4',
+      optionA:'A',
+      optionB:'B',
+      optionC: 'C',
+      optionD: 'D',
+      correctAns:'optionB'
+    
+    },
+    { question:'Question5',
+      optionA:'A',
+      optionB:'B',
+      optionC: 'C',
+      optionD: 'D',
+      correctAns:'optionD'
+    
+    }]
+    
+    module.exports=questions3
